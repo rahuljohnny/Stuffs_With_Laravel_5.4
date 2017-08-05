e1f561eec79ba4292e90f3584403f4960ff7fa2a
Rahul Johnny <royjohnny63@gmail.com>
2017-08-06 01:12:07 +0600

    edited at 6 Aug 12AM

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>{{ $task->body }}</h1> <!--task not tasks -->
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-	$tasks= DB::table('tasks')->get(); 
+Route::get('/tasks'  , function () {
+	$tasks= DB::table('tasks')->latest()->get(); 
 	//return $tasks;
-    return view('welcome', compact('tasks')); //passing the whole array
+    return view('tasks.index', compact('tasks')); //passing the whole array
 });
 
 
-Route::get('/tasks/{id}', function ($id) {
-	//dd($id);
+Route::get('/tasks/{tasks}', function ($id) {
+	// dd($id);
 	$task= DB::table('tasks')->find($id);
-	dd($task);
+	//dd($task);
 	//return $tasks;
-    //return view('welcome', compact('tasks')); //passing the whole array
+    return view('tasks.show', compact('task')); //passing the whole array
 });
