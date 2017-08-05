444adff20fc7de20b17b209f3a09f65c91d0aa8a
Rahul Johnny <royjohnny63@gmail.com>
2017-08-05 06:09:11 +0600

    added by Johnny

--- index55.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Index5</title>
-</head>
-<body>
-
-</body>
-</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <h4>
+  <ul>
+
+
+  
+
+    <?php foreach ($tasks as $task): ?> 
+    <li><?= $task;  ?></li>
+
+    <?php endforeach; ?> 
+
+    </ul>
+</h4>
+</head>
+<body>
+
+</body>
+</html>
